7294081f06ce7cdf6e146371211e92ee42f2e439
Cedrick Ngoma <82066162+ment643@users.noreply.github.com>
2024-05-13 14:57:15 +0200

    adding images
    added images for m project

---
+++ landing_page/src/app/page.js
@@ -0,0 +1,21 @@
+
+
+export default function Home() {
+  return (
+    <div className="h-screen flex  justify-center p-10 bg-black text-white items-center gap-20">
+      <div>
+      <h1 className="text-3xl font-bold bg-red-600">Hello world</h1>
+      <p>maintenance by Daniel</p>
+      </div>
+      <div>
+      <h1 className="text-3xl font-bold bg-red-600 ">website</h1>
+      <p>maintenance by Daniel</p>
+      </div>
+      <div>
+      <h1 className="text-3xl font-bold bg-red-600 ">desciption</h1>
+      <p>maintenance by Daniel</p>
+      </div>
+    </div>
+  )
+}
+

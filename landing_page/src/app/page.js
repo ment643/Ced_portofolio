9564310f025333230f2ff4001cfdde17b1bba9ca
Cedrick Ngoma <82066162+ment643@users.noreply.github.com>
2024-05-22 00:11:00 +0200

    update of the process
    putting some text and description in our portofolio

--- landing_page/src/app/page.js
+++ landing_page/src/app/page.js
@@ -1,21 +1,28 @@
-
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="h-screen flex  justify-center p-10 bg-black text-white items-center gap-20">
-      <div>
-      <h1 className="text-3xl font-bold bg-red-600">Hello world</h1>
-      <p>maintenance by Daniel</p>
-      </div>
-      <div>
-      <h1 className="text-3xl font-bold bg-red-600 ">website</h1>
-      <p>maintenance by Daniel</p>
-      </div>
-      <div>
-      <h1 className="text-3xl font-bold bg-red-600 ">desciption</h1>
-      <p>maintenance by Daniel</p>
-      </div>
-    </div>
+    <div className="h-screen bg-image flex  flex-col ga-p-5">
+    
+    
+ {/*TEXT CONTAINER*/}
+<div className="h-1/2 text-white relative">
+   
+    {/*TITLE*/}
+    <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter p-10">Cedick <br/><span>Ngoma</span></h1>
+
+</div>
+  
+{/*image CONTAINER*/}
+ 
+<div className="h-1/2 text-white p-5 flex flex-col">
+  <Image src="/cedColor.png" alt="" fill className="object-contain p-5"/>
+
+{/*DESCRIPTION*/}
+   <h1 className="font-bold p-10">Web Developer and Production Designer <br/>based in your City</h1>
+</div>
+    
+</div>
   )
 }
 
